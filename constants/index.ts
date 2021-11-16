e96a75b1c2b044304571e54a40d726ebc7e37824
AntonGoncharik <ant.goncharik@gmail.com>
2021-11-17 01:57:44 +0300

    changed url

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://localhost:8081';
 
 export const navbarItems = [
   { key: 'tracks', name: 'Tracks', icon: 'tracks' },
